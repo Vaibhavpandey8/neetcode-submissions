cfc95a382816db5e7ecc2fa01effd658f652cdc2
Vaibhavpandey8 <96644799+Vaibhavpandey8@users.noreply.github.com>
2026-08-13 20:35:07 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++) {
+            // Opening bracket
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            // Closing round bracket
+            else if(s[i] == ')') {
+                if(st.empty() || st.top() != '(') {
+                    return false;
+                }
+                st.pop();
+            }
+            // Closing curly bracket
+            else if(s[i] == '}'){
+                if(st.empty() || st.top() != '{') {
+                    return false;
+                }
+                st.pop();
+            }
+            // Closing square bracket
+            else if(s[i] == ']') {
+                if(st.empty() || st.top() != '[') {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
