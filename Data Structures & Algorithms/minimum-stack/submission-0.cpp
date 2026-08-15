bc0b86c52e626db81c0086246d05fb7a5784a832
Vaibhavpandey8 <96644799+Vaibhavpandey8@users.noreply.github.com>
2026-08-15 22:53:55 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> min;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(min.empty()||val<=min.top()){
+            min.push(val);
+        }
+    }
+    
+    void pop() {
+        if (st.top()==min.top()){
+            min.pop();
+        }
+        st.pop();
+
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+};
