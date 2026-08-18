4ca27492566fa0a44e8779c833e986d0b281ded9
Vaibhav Pandey <96644799+Vaibhavpandey8@users.noreply.github.com>
2026-08-18 15:47:33 +0530

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        int start=0;
+        int end=n-1;
+        while(start<end){
+            char c=s[start];
+            s[start]=s[end];
+            s[end]=c;
+            start++;
+            end--;
+        }
+    }
+};
