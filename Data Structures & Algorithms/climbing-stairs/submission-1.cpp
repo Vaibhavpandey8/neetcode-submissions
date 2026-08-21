d18a5237b262143c4c59b60ca3bd49d7aa97bb0e
Vaibhav Pandey <96644799+Vaibhavpandey8@users.noreply.github.com>
2026-08-21 14:48:16 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<int,int>dp;
+    int climbStairs(int i , int n){
+        if(i==n){
+            return 1;
+        }
+        if(i>n){
+            return 0;
+        }
+        if(dp.find(i)!=dp.end()){
+            return dp[i];
+        }
+        int a1=climbStairs(i+1,n);
+        int a2=climbStairs(i+2,n);
+        int ans=a1+a2;
+        dp[i]=ans;
+        return ans;
+        
+    }
+    int climbStairs(int n) {
+        return climbStairs(0,n);           
+    }
+};
